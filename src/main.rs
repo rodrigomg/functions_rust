3490b2c2d89ad14371a97452e8055101b200e07a
rmgarcia <rockdrigo.mtz@gmail.com>
2018-11-12 16:58:05 -0600

    example for expression

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 fn main() {
     another_function(90);
     mutiple_parameters(23,89.99);
+    expression_sample();
 }
 
 fn another_function(x: i32) {
@@ -12,7 +13,18 @@ fn mutiple_parameters(a: i32, b: f32) {
     println!("The value of b is: {}", b);
 }
 
-/*fn statements_not_return_value(){
+/*fn statements_not_return_value() {
     let x = (let y = 6);
     println!("The value of x is: {}", x);
 }*/
+
+fn expression_sample() {
+    let _a = 5;
+
+    let b = {
+        let a = 10;
+        a + 2
+    };
+
+    println!("The value of b is: {}", b);
+}
