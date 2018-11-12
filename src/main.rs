bc0f9c9363752a33d712f71de644725d87971f35
rmgarcia <rockdrigo.mtz@gmail.com>
2018-11-12 16:47:33 -0600

    example diference between statement and expressions in rust

--- src/main.rs
+++ src/main.rs
@@ -11,3 +11,8 @@ fn mutiple_parameters(a: i32, b: f32) {
     println!("The value of a is: {}", a);
     println!("The value of b is: {}", b);
 }
+
+/*fn statements_not_return_value(){
+    let x = (let y = 6);
+    println!("The value of x is: {}", x);
+}*/
